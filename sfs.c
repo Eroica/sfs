5c25eb62af30fe8044b07cc009a2ce64febf14b0
Eroica <seba79@mailbox.org>
2018-07-12 11:12:29 +0200

    Add `mklink' function

--- sfs.c
+++ sfs.c
@@ -21,7 +21,7 @@ static int l_isdir (lua_State *L) {
 		lua_pushboolean(L, PathIsDirectory(path) == FILE_ATTRIBUTE_DIRECTORY);
 	#else
 		int file = 0;
-        if((file = open(path, O_RDONLY)) == -1)
+		if((file = open(path, O_RDONLY)) == -1)
 			lua_pushboolean(L, false);
 
 		struct stat f_stat;
@@ -39,7 +39,7 @@ static int l_exists (lua_State *L) {
 		lua_pushboolean(L, PathFileExists(path));
 	#else
 		int file = 0;
-        if((file = open(path, O_RDONLY)) == -1)
+		if((file = open(path, O_RDONLY)) == -1)
 			lua_pushboolean(L, false);
 
 		struct stat f_stat;
@@ -69,11 +69,45 @@ static int l_mkpath (lua_State *L) {
 	#endif
 }
 
+static int l_mklink(lua_State *L) {
+	const char *oldpath = luaL_checkstring(L, 1);
+	const char *newpath = luaL_checkstring(L, 2);
+
+	#ifdef _WIN32
+		int symlink = lua_toboolean(L, 3);
+		int is_dir = PathIsDirectory(oldpath) == FILE_ATTRIBUTE_DIRECTORY;
+		if (is_dir && !symlink) {
+				lua_pushnil(L); lua_pushstring(L, "hard links to directories are not supported on Windows");
+				return 2;
+		}
+
+		int result = symlink ? CreateSymbolicLink(newpath, oldpath, is_dir)
+							 : CreateHardLink(newpath, oldpath, NULL);
+		if (result) {
+				return pushresult(L, result, NULL);
+		} else {
+				lua_pushnil(L); lua_pushstring(L, symlink ? "make_link CreateSymbolicLink() failed"
+														  : "make_link CreateHardLink() failed");
+				return 2;
+		}
+	#else
+		int result = (lua_toboolean(L, 3) ? symlink : link)(oldpath, newpath);
+		if (result == 0) {
+			lua_pushboolean(L, 1);
+			return 1;
+		} else {
+			lua_pushboolean(L, 0); lua_pushstring(L, "link creation failed");
+			return 2;
+		}
+	#endif
+}
+
 
 static const struct luaL_Reg sfs_lib [] = {
 	{"is_dir", l_isdir},
 	{"exists", l_exists},
 	{"mkpath", l_mkpath},
+	{"mklink", l_mklink},
 	{NULL, NULL}
 };
 
